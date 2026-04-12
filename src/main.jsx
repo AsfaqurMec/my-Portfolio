26bd74d485e1bf57100693a49a24e5ba1297c074
AsfaqurMec <hamimhamim044@gmail.com>
2026-04-12 18:52:18 +0600

    favicon change

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,12 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes.jsx'
 import { AuthProvider } from './contexts/AuthContext'
 import './index.css'
+import logo from '../public/images/Preview.png'
 
-
+const favicon = document.querySelector('link[rel="icon"]')
+if (favicon) {
+  favicon.href = logo
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
